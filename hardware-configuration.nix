0594c44948a1d5c883d79c20b1d05d1d237bbad5
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-11 19:20:52 +0100

    chore(hardware): update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,13 +4,6 @@
   ...
 }: {
   hardware = {
-    trackpoint = {
-      enable = true;
-      emulateWheel = true;
-      fakeButtons = true;
-      sensitivity = 128;
-      speed = 97;
-    };
     cpu = {
       intel = {
         updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
